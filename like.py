f54ef745bd0261d8bffc646f7f8565df73c2ad91
jtfreedman <babzibles@gmail.com>
2022-06-25 01:20:05 -0400

    small improvements

--- like.py
+++ like.py
@@ -2,18 +2,30 @@
 from time import sleep
 from selenium import webdriver
 
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+# get credentials
 email = input('Enter email: ')
 password = input('Enter password: ')
-# instantiates firefox
-browser = webdriver.Firefox()
+userBrowser = input('Enter your browser: ').lower()
+
+# instantiates browser
+match userBrowser:
+  case 'firefox':
+    browser = webdriver.Firefox()
+  case 'safari':
+    browser = webdriver.Safari()
+  case 'chrome':
+    browser = webdriver.Chrome()
+  case _:
+    print('Your input is not compatibile.')
+    quit()
+
 browser.implicitly_wait(5)
 
-# opens firefox to link
+# opens browser to particular link
 browser.get('https://creator.nightcafe.studio/feed')
 
 # clicks on login by password and logs in
@@ -22,25 +34,17 @@ WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//*[@id
 WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//*[@id='password']"))).send_keys(password)
 WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//span[text()='Log in']"))).click()
 
-# waits for page to load and then navigates to feed
+# waits for page to load and then navigates to specific feed
 sleep(3)
 browser.get('https://creator.nightcafe.studio/explore')
 sleep(3)
 
-# WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//button[@class='css-ifoe43']"))).click()
-# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
+# selects newests posts 
 WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//span[text()='Top: Last Hour']"))).click()
 WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//a[text()='Newest Creations']"))).click()
+
 # continually likes posts so long as there is a visible like button
 sleep(3)
 while (2 > 1):
-  WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//*[text()='Follow']"))).click()
-  try:
-    WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//button[@title='Like']"))).click()
-  except:
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
+  WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, f"//button[@title='Like']"))).click()
   sleep(.75)
-
-#browser.close()
